a90e55bfef7c11ce6b3200edd3474b40692f3f6d
Gabriel Mangeat <gabriel.mangeat@gmail.com>
2018-01-31 09:27:12 -0500

    Copy pasted the licences files on the headers of each functions and minor changes on headers

--- partial_volume_correction/PVC/pvc_compute_volume_fraction.m
+++ partial_volume_correction/PVC/pvc_compute_volume_fraction.m
@@ -22,7 +22,29 @@ function [VP] = pvc_compute_volume_fraction(surf_name, surf_path, number_of_surf
 %       - VP_out.mgz: Partial volume fraction image, created in the current
 %               directory.
 %
-% Original author: Camille Van Assel
+% Function is written by Camille Van Assel Univertsité polytechnique de
+% Montreal (november 2016). 
+% 
+% The MIT License (MIT)
+% Copyright (c) 2016 Ecole Polytechnique, Université de Montréal
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 
 if nargin < 6 
     second_correction = false;
