4ff000ceabdee50f73cfc6836687aa11af75718d
Gabriel Mangeat <gabriel.mangeat@gmail.com>
2018-02-07 11:07:12 -0500

    Changed the folder structure to have a license file per external set source.

---
+++ partial_volume_correction/PVC/ext/Mesh_voxelisation/VOXELISE_example.m
@@ -0,0 +1,38 @@
+%EXAMPLE of the Mesh Voxelisation tool kit.
+%https://www.mathworks.com/matlabcentral/fileexchange/27390-mesh-voxelisation?focused=3777309&tab=function
+%==========================================================================
+
+%Plot the original STL mesh:
+figure
+[stlcoords] = READ_stl('sample.stl');
+xco = squeeze( stlcoords(:,1,:) )';
+yco = squeeze( stlcoords(:,2,:) )';
+zco = squeeze( stlcoords(:,3,:) )';
+[hpat] = patch(xco,yco,zco,'b');
+axis equal
+
+%Voxelise the STL:
+[OUTPUTgrid] = VOXELISE(100,100,100,'sample.stl','xyz');
+
+%Show the voxelised result:
+figure;
+subplot(1,3,1);
+imagesc(squeeze(sum(OUTPUTgrid,1)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('Y-direction');
+axis equal tight
+
+subplot(1,3,2);
+imagesc(squeeze(sum(OUTPUTgrid,2)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('X-direction');
+axis equal tight
+
+subplot(1,3,3);
+imagesc(squeeze(sum(OUTPUTgrid,3)));
+colormap(gray(256));
+xlabel('Y-direction');
+ylabel('X-direction');
+axis equal tight
